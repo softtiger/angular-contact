40536d42df1df6101f5c596efe4e61d2fc6c959a
softtiger <revokeyu@hotmail.com>
2020-08-11 17:28:40 +0800

    init http request enviroment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule}  from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -35,6 +36,7 @@ import { LayoutComponent } from './layout/layout.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     FormsModule
   ],
--- src/app/sigup/sigup.component.ts
+++ src/app/sigup/sigup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient}  from '@angular/common/http';
 
 @Component({
   selector: 'app-sigup',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigupComponent implements OnInit {
 
-  constructor() { }
+  signupForm:{email:string,password:string} = { email:'',password:''}
+
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  sigup(){
+     const formData = this.signupForm;
+     console.log(formData)
+     this.http.post("http://localhost:3000/users",formData).toPromise().then(data => console.log(data));
   }
 
 }
